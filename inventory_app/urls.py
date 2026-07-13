ed676bf73264d39f75e90ca27ea10fb131f672d6
Nithin <nithingudditi@gmail.com>
2026-07-14 00:46:05 +0530

    Add Product create, edit, delete views and templates

--- inventory_app/urls.py
+++ inventory_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('products/', views.product_list, name='product_list'),
+    path('products/add/', views.product_create, name='product_create'),
+    path('products/<int:pk>/edit/', views.product_update, name='product_update'),
+    path('products/<int:pk>/delete/', views.product_delete, name='product_delete'),
 ]
